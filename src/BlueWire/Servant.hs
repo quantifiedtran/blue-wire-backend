2a7976eb583c2f6402fc4199f9ca334c65925dd6
quantifiedtran <mari.harald.u.k@gmail.com>
2016-10-19 15:54:22 +0100

    Added servant types for eventual migration of framework

---
+++ src/BlueWire/Servant.hs
@@ -0,0 +1,38 @@
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE DataKinds                  #-}
+{-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
+{-# LANGUAGE TypeOperators              #-}
+{-|
+    Module for blue-wire API expressed in Servant.
+-}
+module BlueWire.Servant where
+
+import Servant.API
+import BlueWire.Types
+import BlueWire.Database.Schema
+import BlueWire.Database.Query
+import Data.Time
+import Data.Proxy
+
+-- | The api for looking at and modifying `AppProfile`s in the database.
+type AppAPI =                 Get  '[JSON] AppProfile
+          :<|> "heartbeat" :> Post '[JSON] HeartbeatResponse
+          :<|> "kick"      :> KickAPI
+
+-- | The api for looking at and modifying `Kick`s specifically.
+type KickAPI = Get '[JSON] [Kick]
+          :<|> ReqBody '[JSON] [Kick] :> Post '[JSON] UTCTime
+          :<|> "add" :> ReqBody '[JSON] [Kick] :> Post '[JSON] [Kick]
+
+{-|
+    The API type for blue-wire.
+-}
+type BlueWireAPI str config
+      =  "new"       :> ReqBody '[JSON] AppProfile :> Post '[JSON]      UTCTime
+    :<|> "app"       :> Capture "application" str  :> AppAPI
+    :<|> "config"                                  :> Get  '[JSON]      config
