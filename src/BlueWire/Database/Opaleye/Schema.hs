2bc3c813f1f73b0773056aaa31383ff6073acf6e
quantifiedtran <mari.harald.u.k@gmail.com>
2017-06-15 16:25:13 +0100

    Began work on moving to Opaleye from Persistant

---
+++ src/BlueWire/Database/Opaleye/Schema.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+module BlueWire.Database.Opaleye.Schema where
+
+import Opaleye
+import Data.Typeable
+import Data.Generics
+import Data.Time
+import Data.Profunctor.Product.TH
+
+data Recovery r h mx = Recovery {
+    rate :: r,
+    hurdle :: h,
+    maxTime :: mx
+} deriving (Eq, Ord, Show, Data, Typeable)
+
+$(makeAdaptorAndInstance "pRecovery" ''Recovery)
+type Recovery' = Recovery NominalDiffTime NominalDiffTime NominalDiffTime
+
+data Kick dur ctdn repct rec = Kick {
+    duration :: dur,
+    countdown :: ctdn,
+    repeatCountdown :: repct,
+    recoveryProfile :: rec
+} deriving (Eq, Ord, Show, Data, Typeable)
+
+$(makeAdaptorAndInstance "pKick" ''Kick)
+type Kick' = Kick NominalDiffTime NominalDiffTime (Maybe NominalDiffTime) (Maybe Recovery')
+
+data Profile nm actkck lsthb kckends cnsk = Profile {
+    name :: nm,
+    activeKicks :: actkck,
+    lastHeartbeat :: lsthb,
+    kickEnds :: kckends,
+    canNextSetKicks :: cnsk
+} deriving (Eq, Ord, Show, Data, Typeable)
+
+$(makeAdaptorAndInstance "pProfile" ''Profile)
